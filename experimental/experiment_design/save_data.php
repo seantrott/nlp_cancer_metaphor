955e5a8eaec90622a604999c57c8830441c6eab6
Sean Trott <seantrott@Seans-MacBook-Pro-8.local>
2019-06-19 10:34:31 -0700

    save data

--- experimental/experiment_design/save_data.php
+++ experimental/experiment_design/save_data.php
@@ -1,7 +1,7 @@
 <?php
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
-$filename = "/home/sttrott/web-data".$_POST['filename'];
+$filename = "/home/sttrott/web-data/".$_POST['filename'];
 $data = $_POST['filedata'];
 // write the file to disk
 file_put_contents($filename, $data);
