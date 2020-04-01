25e6d5499c7d9a28529e53583faa26389cc9c798
Alex Liebscher <alexliebscher0@gmail.com>
2020-04-01 12:11:08 -0700

    Refactor experiment files

---
+++ experimental/clean_data.R
@@ -0,0 +1,97 @@
+library(tidyverse)
+library(jsonlite)
+
+# set the working directory to the project base directory
+
+setwd("experimental/")
+
+read = function (x) {
+  #, Read the raw data from the experiment (from .csv form)
+  
+  read_csv(paste("data/raw/", x, sep=""), col_types = cols())
+}
+
+# iterate over all raw .csv files and collate data
+data =
+  list.files("data/raw/", pattern="*.csv") %>%
+  map_df(~read(.))
+
+# retain only useful columns
+dataf = data[, c("rt", "time_elapsed", "ppt", "recipient_sex", "metaphor", "button_pressed", "response", "responses", "qtype")]
+
+# group by participant, then create the relevant columns
+dataf = dataf %>%
+  group_by(ppt) %>%
+  summarise(
+    donation = response[qtype %in% "trial"],
+    cond_sex = recipient_sex[1],
+    cond_metaphor = metaphor[1],
+    rt_consent = rt[1], # RT of consent form, in milliseconds
+    rt_check1 = rt[2],
+    rt_check2 = rt[3],
+    rt_instructions = rt[4], # RT of instruction pages, in milliseconds
+    rt_trial = rt[5], # RT of experimental trial, in milliseconds
+    rt_description = rt[6], # (all below, follow-up questions) in milliseconds
+    rt_pastdonations = rt[7],
+    rt_followup = rt[8],
+    rt_cancer = rt[9],
+    rt_demographic = rt[10],
+    rt_age = rt[11],
+    rt_purpose = rt[12],
+    rt_feedback = rt[13],
+    rt_debrief = rt[14], # RT of debrief form, in milliseconds
+    total_time = time_elapsed[14], # total time in milliseconds
+    check1 = button_pressed[qtype %in% "check1"],
+    check2 = button_pressed[qtype %in% "check2"],
+    description = fromJSON(responses[qtype %in% "description"])$Q0,
+    past_donations = fromJSON(responses[qtype %in% "past-donations"])$Q0,
+    urgent = fromJSON(responses[qtype %in% "urgent"])$Q0,
+    sympathy = fromJSON(responses[qtype %in% "sympathy"])$Q0,
+    self_cancer = factor(fromJSON(responses[qtype %in% "self-cancer"])$Q0, 
+                         levels = c('Yes', 'No', 'Prefer not to say'), 
+                         labels = c("Y", "N", "NA")),
+    ff_cancer = factor(fromJSON(responses[qtype %in% "ff-cancer"])$Q0, 
+                       levels = c('Yes', 'No', 'Prefer not to say'), 
+                       labels = c("Y", "N", "NA")),
+    gender = factor(fromJSON(responses[qtype %in% "demographics"])$Q0, 
+                    levels = c("Male", "Female", "Non-binary", "Prefer not to say"), 
+                    labels = c("M", "F", "NB", "NA")),
+    education = factor(fromJSON(responses[qtype %in% "demographics"])$Q1, 
+                    levels = c('Less than a high school diploma',
+                               'High school degree or equivalent (e.g. GED)',
+                               'Associate degree (e.g. AA, AS)',
+                               'Bachelor’s degree (e.g. BA, BS)',
+                               'Master’s degree (e.g. MA, MS, MEd)',
+                               'Professional degree (e.g. MD, DDS, DVM)',
+                               'Doctorate (e.g. PhD, EdD)',
+                               'Prefer not to say'), 
+                    labels = c("<HS", "HS", "A", "B", "M", "P", "D", "NA")),
+    socioeconomic = factor(fromJSON(responses[qtype %in% "demographics"])$Q2, 
+                    levels = c('Less than $10,000',
+                               '$10,000 through $24,999',
+                               '$25,000 through $49,999',
+                               '$50,000 through $74,999',
+                               '$75,000 through $99,999',
+                               '$100,000 through $149,999',
+                               'More than $150,000',
+                               'Prefer not to say'),
+                    labels = c("<10k", "10-25k", "25-50k", "50-75k", "75-100k", "100-150k", ">150k", "NA")),
+    english = factor(fromJSON(responses[qtype %in% "demographics"])$Q3, 
+                    levels = c('Yes', 'No', 'Prefer not to say'), 
+                    labels = c("Y", "N", "NA")),
+    age = fromJSON(responses[qtype %in% "age"])$Q0,
+    purpose = fromJSON(responses[qtype %in% "purpose"])$Q0,
+    feedback = fromJSON(responses[qtype %in% "feedback"])$Q0
+  )
+
+# convert past donations to intergers, coercing non-integers to NAs
+dataf$past_donations = as.integer(dataf$past_donations)
+
+# convert age inputs to integers
+dataf$age = as.integer(dataf$age)
+# if anyone entered there age as > 1900, assume they typed their birth year and convert by subtracting that from the current year
+m = dataf$age > 1900 & !is.na(dataf$age)
+dataf[m, ]$age = 2020 - dataf[m, ]$age
+
+# save the data frame to a cleaned .csv
+write_csv(dataf, "data/data_clean.csv")
